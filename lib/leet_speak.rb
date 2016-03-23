a9b80686d8da8f39eccb143690edf837e099e2bf
chetan.guevara <chetan.nandakumar@gmail.com>
2016-03-23 15:45:12 -0700

    first commit

---
+++ lib/leet_speak.rb
@@ -0,0 +1,15 @@
+require('pry.rb')
+class String
+  define_method(:leet_speak) do
+    split_sentence = self.split
+    split_sentence.each do |word|
+      #spell out all the rules 
+      word.gsub!('e','3')
+      word.gsub!('o', '0')
+      word.gsub!('I','1')
+      size = word.length()
+      word[1..size].gsub!('s','z')
+    end
+    split_sentence.join(" ")
+  end
+end
