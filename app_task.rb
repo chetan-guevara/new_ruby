3c1c1262452c85137d18e4c59fe905b070c90684
chetan.guevara <chetan.nandakumar@gmail.com>
2016-03-31 15:55:11 -0700

    ToDo App working with postgres DB

--- app_task.rb
+++ app_task.rb
@@ -1,7 +1,10 @@
 require("sinatra")
 require("sinatra/reloader")
 #also_reload("lib/**/*.rb")
+require('pg')
 require("./lib/task")
+
+DB = PG.connect({:dbname => 'to_do'})
 
 get("/") do
   @tasks = Task.all()
@@ -10,7 +13,7 @@ end
 
 post("/tasks") do
   description = params.fetch("description")
-  task = Task.new(description)
+  task = Task.new({:description => description, :list_id => 1})
   task.save()
   erb(:success)
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,12 +2,12 @@ require('rspec')
 require('anagram')
 
 describe('String#anagram?') do
-  it("returns true if the inputs are anagrams of one another") do
-    expect("rate".anagram?('tear')).to(eq(true))
-  end
-  it("returns false if the inputs are not anagrams of one another") do
-    expect("rate".anagram?('great')).to(eq(false))
-  end
+	it("returns true if the inputs are anagrams of one another") do
+		expect("rate".anagram?('tear')).to(eq(true))
+	end
+	it("returns false if the inputs are not anagrams of one another") do
+		expect("rate".anagram?('great')).to(eq(false))
+	end
 end
 
 
